c3708ff1715e3cb1b28c47a0d92b48282b99f552
Andres Mestra <andresmestra11a@gmail.com>
2022-04-17 20:44:51 -0500

    feat: change color items statusbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
-import 'package:desing/src/screens/home_screen.dart';
-import 'package:desing/src/screens/scroll_design.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:desing/src/screens/home_screen.dart';
 import 'package:desing/src/screens/basic_design.dart';
+import 'package:desing/src/screens/scroll_design.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    //change color items statusbar
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
