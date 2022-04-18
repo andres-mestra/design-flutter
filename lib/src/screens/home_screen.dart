11b3c7d51649291b93b545f7b1840ffc67615baf
Andres Mestra <andresmestra11a@gmail.com>
2022-04-17 20:46:35 -0500

    feat: page titles

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:desing/src/widgets/background.dart';
+import 'package:desing/src/widgets/page_title.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -10,7 +11,26 @@ class HomeScreen extends StatelessWidget {
       body: Stack(
         children: [
           //Background
-          Background()
+          Background(),
+          _HomeBody()
+        ],
+      ),
+    );
+  }
+}
+
+class _HomeBody extends StatelessWidget {
+  const _HomeBody({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          //Titles
+          PageTitle()
         ],
       ),
     );
